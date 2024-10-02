8c07a78dd974a9e03d9094e5cb232bb7c233340d
viktor7ltz <78755786+viktor7ltz@users.noreply.github.com>
2024-10-02 11:38:44 +0200

    hero change in width

--- dist/js/modularity-alingsashero.9a4d1aa7a4ec2da652a1.js
+++
@@ -1,8 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/*!**********************************************!*\
-  !*** ./source/js/modularity-alingsashero.js ***!
-  \**********************************************/
-console.log('Alingsas Hero'); 
-/******/ })()
-;
-//# sourceMappingURL=modularity-alingsashero.9a4d1aa7a4ec2da652a1.js.map
---
+++ dist/js/modularity-alingsashero.a448f42d8aa0e6518b7a.js
@@ -0,0 +1,2 @@
+console.log("Alingsas Hero");
+//# sourceMappingURL=modularity-alingsashero.a448f42d8aa0e6518b7a.js.map
